99595979b25a1a1c885ad49af80084a5fd425c61
William Le <mrwillyle@gmail.com>
2015-10-17 16:27:30 -0400

    updated the models.rb file. not final

--- models.rb
+++ models.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 	has_one :profile
 	has_many :posts
 	has_many :comments
+	has_many :followers, through: :userfollowers
 end
 class Profile < ActiveRecord::Base
 	belongs_to :user
@@ -16,7 +17,10 @@ class Comment < ActiveRecord::Base
 	belongs_to :comment
 	has_many :comments
 end
-# class UserFollow < ActiveRecord:Base
-# 	belongs_to :user
-# 	belongs_to :follower
-# end
+class Follower < User
+	has_many :users, through: :userfollowers
+end
+class UserFollower < ActiveRecord:Base
+	belongs_to :user
+	belongs_to :follower
+end
